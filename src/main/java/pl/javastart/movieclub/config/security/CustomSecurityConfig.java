8a91607d3b10bda0922dbfce50ca7d37f884b01c
Krystian <krystian14d@gmail.com>
2022-06-24 10:15:44 +0200

    logout configuration

--- src/main/java/pl/javastart/movieclub/config/security/CustomSecurityConfig.java
+++ src/main/java/pl/javastart/movieclub/config/security/CustomSecurityConfig.java
@@ -2,10 +2,11 @@ package pl.javastart.movieclub.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class CustomSecurityConfig {
@@ -15,19 +16,24 @@ public class CustomSecurityConfig {
     private static final String ADMIN_ROLE = "ADMIN";
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests((authz) -> authz
-                .mvcMatchers("/admin/**").hasAnyRole(EDITOR_ROLE, ADMIN_ROLE)
-                .anyRequest().permitAll()
-        )
+                        .mvcMatchers("/admin/**").hasAnyRole(EDITOR_ROLE, ADMIN_ROLE)
+                        .anyRequest().permitAll()
+                )
                 .formLogin(login -> login
                         .loginPage("/login")
-                        .permitAll());
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout/**", HttpMethod.GET.name()))
+                        .logoutSuccessUrl("/login?logout").permitAll()
+                );
         return http.build();
     }
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer(){
+    public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring().mvcMatchers(
                 "/img/**",
                 "/scripts/**",
